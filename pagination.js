20c71fec78427432bab2f121ccb955539ec310cc
Bryan Mailloux <Bryan.Mailloux@D2L.com>
2020-08-15 17:47:41 -0400

    Add tests

--- pagination.js
+++ pagination.js
@@ -58,6 +58,7 @@ class Pagination extends RtlMixin(Localizer(LitElement)) {
 		this.maxPageNumber = 1;
 	}
 
+	// TODO: page number types are inconsistent depending on if event is fired from arrows or from Enter keypress
 	_submitPageNumber(e) {
 		if (!this._isValidNumber(e.target.value)) {
 			e.target.value = this.pageNumber.toString();
